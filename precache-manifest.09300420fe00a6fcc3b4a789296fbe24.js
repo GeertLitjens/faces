878d7c3682dbca82426a310f5f3e82b6fde0005a
Geert Litjens <gjslitjens@gmail.com>
2018-11-30 11:44:48 +0100

    Updates

---
+++ precache-manifest.09300420fe00a6fcc3b4a789296fbe24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "694dcd0a5422f18f9ffa8d513651aefc",
+    "url": "/metastaticcellclassifier/index.html/static/media/kankercellen_000.694dcd0a.jpg"
+  },
+  {
+    "revision": "c3dcfb8f7a885127c607",
+    "url": "/metastaticcellclassifier/index.html/static/js/runtime~main.c3dcfb8f.js"
+  },
+  {
+    "revision": "8a418872733b15e5a055",
+    "url": "/metastaticcellclassifier/index.html/static/js/main.8a418872.chunk.js"
+  },
+  {
+    "revision": "46afb3104ac88956d849",
+    "url": "/metastaticcellclassifier/index.html/static/js/1.46afb310.chunk.js"
+  },
+  {
+    "revision": "8a418872733b15e5a055",
+    "url": "/metastaticcellclassifier/index.html/static/css/main.e7e71074.chunk.css"
+  },
+  {
+    "revision": "46afb3104ac88956d849",
+    "url": "/metastaticcellclassifier/index.html/static/css/1.88a99f69.chunk.css"
+  },
+  {
+    "revision": "b45586ff9505cc276d994137f1ff9c41",
+    "url": "/metastaticcellclassifier/index.html/index.html"
+  }
+];
